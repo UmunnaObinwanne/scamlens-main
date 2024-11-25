0182b25c5d467cb69c5f167a8240d24cba3c7d7b
Mike <broadwaymarketingconsults@gmail.com>
2024-11-26 00:40:00 +0100

    callback error fix

--- src/app/api/auth/kinde_callback/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/auth/kinde_callback/route.ts
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const authResponse = await handleAuth()(request);
-  return authResponse;
-}
